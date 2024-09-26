3ead8f3d5a4b654f37922f4f12e84503b580457d
Walt Jones <wjones@sram.com>
2024-09-26 14:56:12 -0600

    adding link

---
+++ src/text_6bit.h
@@ -0,0 +1,15 @@
+#ifndef TEXT_6BIT_H
+#define TEXT_6BIT_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_LENGTH 280
+
+const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 =";
+
+char* text_to_binary(const char* text);
+
+#endif /*SEND_H*/
