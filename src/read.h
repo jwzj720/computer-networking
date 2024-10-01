df348284ede5e2326fa2bf89bf12780e6a1d5ba2
Walt Jones <w_jones@coloradocollege.edu>
2024-10-01 15:29:14 -0600

    fixed header

--- src/read.h
+++ src/read.h
@@ -1,17 +1,29 @@
 #ifndef READ_H
 #define READ_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include <pigpiod_if2.h>
+#include <stdio.h>
 #include <string.h>
-#include "transmission.h"
+#include <stdlib.h>
+#include <inttypes.h>
 
 #define MAX_BITS 700
 #define BIT_COUNT 7
 
-char* read_bits(int GPIO_SEND, int GPIO_RECEIVE);
+struct ReadData{
+    uint32_t READRATE;
+    uint32_t ptime;
+    uint32_t tick1;
+    int rateset;
+    int counter;
+    int values;
+    int run;
+    char* data;
+};
+
+struct ReadData* create_reader();
+void reset_reader(struct ReadData* rd);
+void get_bit(int pi, unsigned gpio, unsigned level, uint32_t tick, void* user);
+char* read_bits(struct ReadData* rd); /* An example function declaration */
 
 #endif
