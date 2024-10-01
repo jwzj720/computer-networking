fbb0e4d71ef910f802c73c7b0a27930f8f14f033
Stuart Sessions <s_sessions@coloradocollege.edu>
2024-09-30 22:12:33 -0600

    progress report and updates to receive stuff

--- src/link_receive.h
+++ src/link_receive.h
@@ -8,18 +8,10 @@
 #include <inttypes.h>
 
 #include "text_7bit.h"
-<<<<<<< HEAD
 #include "read.h"
-=======
-
->>>>>>> c2d5ea376abc7551edae591c69ed932c0ecf0a24
 #define GPIO_SEND 23
 #define GPIO_RECEIVE 26
 
 int read_to_file();
 
-<<<<<<< HEAD
 #endif /*SEND_H*/
-=======
-#endif /*SEND_H*/
->>>>>>> c2d5ea376abc7551edae591c69ed932c0ecf0a24
