61f88b3aa9549a0afd0398e75204f05ee619ba3c
Walt Jones <w_jones@coloradocollege.edu>
2024-10-01 11:22:56 -0600

    adding link layer header file with ports for send

--- src/link_layer.h
+++ src/link_layer.h
@@ -13,9 +13,9 @@
 #include "link_receive.h"
 #include "link_send.h"
 
-char* listen_message(int GPIO_SEND, int GPIO_RECEIVE); // this method will be used to open a listening port and wait for a message
+char* listen_message(int port); // this method will be used to open a listening port and wait for a message
 // to be recieved and then return the message as a string
-int send_message(char *bitstring); // this method will be used to send a message to another device
+int send_message(char *bitstring, int port); // this method will be used to send a message to another device
 
 
 #endif /*LINK_LAYER_H*/
